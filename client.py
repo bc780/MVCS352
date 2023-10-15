c136c3a48599d45ef2e75d0bb196c51bd6a60139
XanderKasternakis <147191291+XanderKasternakis@users.noreply.github.com>
2023-10-15 10:48:35 -0400

    Update client.py

--- client.py
+++ client.py
@@ -12,8 +12,26 @@ print(serverPort)
 print(msgName)
 print(sigName)
 
+msgSizes = []
+msgBytes = []
+signatures = []
+
+with open(msgName, 'r', encoding='ascii') as file:
+    for i, line in enumerate(fp):
+        if i%2==0:
+            msgSizes.append(int(line.strip()))
+        else:
+            bytes = bytes(line.strip(),'ascii')
+            msgBytes.append(bytes) 
+          
+with open(sigName, 'r', encoding='ascii') as file:
+    for i, line in enumerate(fp):
+        signatures.append(line.strip())
+          
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((serverName, serverPort))
+
+s.send("HELLO")
 
 msg = s.recv(128)
 print(msg.decode("utf-8"))
