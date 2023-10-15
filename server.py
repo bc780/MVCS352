183b98fce2908e9ec5e797250eab8cfc0cf4731c
XanderKasternakis <147191291+XanderKasternakis@users.noreply.github.com>
2023-10-15 10:51:47 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from fileinput import filename
 import socket, sys
 
-
 # take in arguments
 listenPort = int(sys.argv[1])
 keyName = sys.argv[2]
@@ -9,6 +8,12 @@ keyName = sys.argv[2]
 #temp debug to check args
 print(listenPort)
 print(keyName)
+
+keys = []
+
+with open(keyName, 'r', encoding='ascii') as file:
+    for i, line in enumerate(file):
+        keys.append(line.strip())
 
 #establish socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
